8373ab604ab2f7329973310d4c8945ac02454a20
Antonio Sitoe <72309855+Antonio-Sitoe@users.noreply.github.com>
2021-11-30 09:17:16 +0200

    fix menu mobile

--- js/Modules/menuMobile.js
+++ js/Modules/menuMobile.js
@@ -4,7 +4,7 @@ const menuMobile = () => {
     certification = document.querySelector('#btnMenu'),
     modal = document.querySelector('.modalbg'),
     closeSpan = document.querySelector('#modalspan');
-  const services = document.querySelectorAl('#services');
+  const services = document.querySelectorAll('#services');
 
   function openMenu() {
     if (menu) menu.classList.toggle('active');
@@ -17,8 +17,10 @@ const menuMobile = () => {
   if (certification) certification.addEventListener('click', openModal);
   if (closeSpan) closeSpan.addEventListener('click', openModal);
   if (services)
-    services.addEventListener('click', () => {
-      if (menu) menu.classList.remove('active');
+    services.forEach((service) => {
+      service.addEventListener('click', () => {
+        if (menu) menu.classList.remove('active');
+      });
     });
 };
 
